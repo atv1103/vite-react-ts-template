db3aa55a0a76cfdf0daad9e6ee7eccd8ffa92e4e
Tatiana Anuchina <1103atv@gmail.com>
2024-08-20 20:04:45 +1000

    add docker, add hot reload settings

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+### Vite HoteReload trouble https://github.com/vitejs/vite/discussions/16102
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000, // must be a port other than 5173
+    host: true, 
+    watch: {
+      usePolling: true,
+    },
+  },
 })
+
+// Vite HoteReload trouble https://github.com/vitejs/vite/discussions/16102
