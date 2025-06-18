de06a9beb99639dc19cf02ffc0cf41ac89ea1978
Tatiana Anuchina <1103atv@gmail.com>
2025-06-19 01:25:22 +1000

    update ESLint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,35 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ['./tsconfig.app.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react-refresh'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    
+    'react/react-in-jsx-scope': 'off', // не нужен в проектах на Vite/Next
+    '@typescript-eslint/no-unused-vars': ['warn'],
   },
 }
