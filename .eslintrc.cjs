b7b0e6a1ff4225e65f2f6f66be888aa15800d926
Tatiana Anuchina <1103atv@gmail.com>
2025-06-19 02:11:59 +1000

    add Prettier, update ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,15 +21,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs', '*.config.js'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    
     'react/react-in-jsx-scope': 'off', // не нужен в проектах на Vite/Next
-    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // разрешает аргументы, начинающиеся с _, даже если они не используются
+    'no-console': 'warn',
+    'no-debugger': 'warn',
   },
 }
